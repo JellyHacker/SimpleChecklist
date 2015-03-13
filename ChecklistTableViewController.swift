45750795b6e7470d24f10d50c81fe8589c912c3c
Brandon Evans <brandon.evans.uw@gmail.com>
2015-03-13 12:10:03 -0500

    Move save/load functionality to model

--- ChecklistTableViewController.swift
+++ ChecklistTableViewController.swift
@@ -11,8 +11,9 @@ import UIKit
 class ChecklistTableViewController: UITableViewController, ItemDetailViewControllerDelegate {
     
     var checklist: Checklist!
-    var items: [ChecklistItem]
+    var items: [ChecklistItem] = []
     
+    /*
     required init(coder aDecoder: NSCoder) {
         
         items = [ChecklistItem]()
@@ -51,7 +52,7 @@ class ChecklistTableViewController: UITableViewController, ItemDetailViewControl
         println("Data file path is \(dataFilePath())")
         */
     }
-
+    */
     override func viewDidLoad() {
         
         super.viewDidLoad()
@@ -65,13 +66,13 @@ class ChecklistTableViewController: UITableViewController, ItemDetailViewControl
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // Return the number of rows in the section.
-        return items.count
+        return checklist.items.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCellWithIdentifier("ChecklistItem") as UITableViewCell
-        let item = items[indexPath.row]
+        let item = checklist.items[indexPath.row]
         let label = cell.viewWithTag(1000) as UILabel
         
         label.text = item.text
@@ -83,22 +84,20 @@ class ChecklistTableViewController: UITableViewController, ItemDetailViewControl
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         if let cell = tableView.cellForRowAtIndexPath(indexPath) {
-            let item = items[indexPath.row]
+            let item = checklist.items[indexPath.row]
             
             item.toggleChecked()
             configureCheckmarkForCell(cell, withChecklistItem: item)
         }
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
-        saveChecklistItems()
     }
     
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         
         let indexPaths = [indexPath]
         
-        items.removeAtIndex(indexPath.row)
+        checklist.items.removeAtIndex(indexPath.row)
         tableView.deleteRowsAtIndexPaths(indexPaths, withRowAnimation: .Automatic)
-        saveChecklistItems()
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
@@ -120,7 +119,7 @@ class ChecklistTableViewController: UITableViewController, ItemDetailViewControl
             
             if let indexPath = tableView.indexPathForCell(sender as UITableViewCell) {
                 // Sets itemToEdit equal to the item in the items array that has the row matching that which was selected in the tableView
-                controller.itemToEdit = items[indexPath.row]
+                controller.itemToEdit = checklist.items[indexPath.row]
             }
         }
     }
@@ -149,9 +148,9 @@ class ChecklistTableViewController: UITableViewController, ItemDetailViewControl
     
     func itemDetailViewController(controller: ItemDetailViewController, didFinishAddingItem item: ChecklistItem) {
        
-        let newRowIndex = items.count
+        let newRowIndex = checklist.items.count
         
-        items.append(item)
+        checklist.items.append(item)
         
         let indexPath = NSIndexPath(forRow: newRowIndex, inSection: 0)
         let indexPaths = [indexPath]
@@ -159,12 +158,11 @@ class ChecklistTableViewController: UITableViewController, ItemDetailViewControl
         tableView.insertRowsAtIndexPaths(indexPaths, withRowAnimation: .Automatic)
         
         dismissViewControllerAnimated(true, completion: nil)
-        saveChecklistItems()
     }
     
     func itemDetailViewController(controller: ItemDetailViewController, didFinishEditingItem item: ChecklistItem) {
         
-        if let index = find(items, item) {
+        if let index = find(checklist.items, item) {
             
             let indexPath = NSIndexPath(forRow: index, inSection: 0)
             
@@ -174,57 +172,8 @@ class ChecklistTableViewController: UITableViewController, ItemDetailViewControl
             }
         }
         dismissViewControllerAnimated(true, completion: nil)
-        saveChecklistItems()
     }
     
-    func documentsDirectory() -> String {
-        
-        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true) as [String]
-        
-        return paths[0]
-    }
-    
-    func dataFilePath() -> String {
-        
-        return documentsDirectory().stringByAppendingPathComponent("Checklists.plist")
-    }
-    
-    /*
-    This method takes the contents of the items array and in two steps converts this to a block of binary data and then writes this data to a file:
-    1. NSKeyedArchiver, which is a form of NSCoder that creates plist files, encodes the array and all the ChecklistItems in it into some sort of binary data format that can be written to a file.
-    2. That data is placed in an NSMutableData object, which will write itself to the file specified by dataFilePath.
-    */
-    func saveChecklistItems() {
-        
-        let data = NSMutableData()
-        let archiver = NSKeyedArchiver(forWritingWithMutableData: data)
-        
-        archiver.encodeObject(items, forKey: "ChecklistItems")
-        archiver.finishEncoding()
-        data.writeToFile(dataFilePath(), atomically: true)
-    }
-    
-    func loadChecklistItems() {
-        
-        // First you put the results of [self dataFilePath] in a temporary constant named path. You use the pathname more than once in this method so having it available in a local instead of calling dataFilePath() several times over is a small optimization.
-        let path = dataFilePath()
-        
-        // Then you check whether the file actually exists and decide what happens based on that. If there is no Checklists.plist then there are obviously no ChecklistItem objects to load. This is what happens when the app is started up for the very first time. In that case, you’ll skip the rest of this method.
-        if NSFileManager.defaultManager().fileExistsAtPath(path) {
-        
-            //When the app does find a Checklists.plist file, you’ll load the entire array and its contents from the file. This is essentially the reverse of saveChecklistItems(). First you load the contents of the file into an NSData object. Because this may fail, you put it in an if let statement. Then you create an NSKeyedUnarchiver object (note: this is an unarchiver) and ask it to decode that data into the items array. This populates the array with exact copies of the ChecklistItem objects that were frozen into this file.
-            if let data = NSData(contentsOfFile: path) {
-            
-            let unarchiver = NSKeyedUnarchiver(forReadingWithData: data)
-            
-            items = unarchiver.decodeObjectForKey("ChecklistItems") as [ChecklistItem]
-            unarchiver.finishDecoding()
-            }
-            
-        }
-
-    }
-  
     
     
     
