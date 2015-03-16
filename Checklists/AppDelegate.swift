b3886555506eff3bf026bd70f1d91ddff25b8e8f
Brandon Evans <brandon.evans.uw@gmail.com>
2015-03-16 18:56:48 -0500

    Updated saveData method

--- Checklists/AppDelegate.swift
+++ Checklists/AppDelegate.swift
@@ -51,9 +51,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func saveData() {
         
-        let navigationController = window!.rootViewController as UINavigationController
-        let controller = navigationController.viewControllers[0] as AllListsViewController
-        
         dataModel.saveChecklists()
     }
     
